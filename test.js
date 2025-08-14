0e13d1ebecbf2fb54e5bbe09512b0b96444eae9b
Igor Kh <igor150195@gmail.com>
2025-08-14 22:53:23 +0500

    тестирую уже сам

--- test.js
+++ test.js
@@ -2,3 +2,4 @@ console.log('test')
 console.log("Это изменение из ветки feature/conflict-test");
 console.log("Это изменение из ветки master");
 console.log("Изменение в master");
+console.log(12312)
