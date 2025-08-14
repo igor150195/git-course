a1dba59a3023b4e01a60670acdd31114ad417c99
Igor Kh <igor150195@gmail.com>
2025-08-14 22:55:52 +0500

    тест конлифкта два

--- test.js
+++ test.js
@@ -2,3 +2,4 @@ console.log('test')
 console.log("Это изменение из ветки feature/conflict-test");
 console.log("Это изменение из ветки master");
 console.log("Изменение в conflict-test");
+console.log(123123222)
