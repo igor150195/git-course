53351eafdebae8ca075fbf0657ca955085524e22
igor150195 <igor150195@gmail.com>
2025-08-15 09:17:21 +0500

    внёс правку в test.js

--- test.js
+++ test.js
@@ -3,3 +3,5 @@ console.log("Это изменение из ветки feature/conflict-test");
 console.log("Это изменение из ветки master");
 console.log("Изменение в conflict-test");
 console.log(123123222)
+
+console.log(222)
